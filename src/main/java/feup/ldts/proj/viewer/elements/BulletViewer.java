f656ff913660d3873f63b2235e35764dfc7af69e
Pedro <pedroramalhoese@gmail.com>
2022-01-06 15:12:55 +0000

    Add: MonsterViewerTest.java, tests for our MonsterViewer

--- src/main/java/feup/ldts/proj/viewer/elements/BulletViewer.java
+++ src/main/java/feup/ldts/proj/viewer/elements/BulletViewer.java
@@ -5,7 +5,7 @@ import feup.ldts.proj.gui.GUI;
 import feup.ldts.proj.model.elements.Bullet;
 
 public class BulletViewer implements ElementViewer<Bullet> {
-    final String
+    final String //to be changed
             BULLET_COLOR_INI = Game.Colors.get("Golden"),
             BULLET_COLOR_MED = Game.Colors.get("SlightRust"),
             BULLET_COLOR_END = Game.Colors.get("Rust");
